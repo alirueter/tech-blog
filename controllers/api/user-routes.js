e6a0bf3b88395e96f153e1cb446a4f859efc90dd
alirueter <rueter.alison@gmail.com>
2020-08-25 17:44:05 -0500

    file structure, started routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// GET all users (/api/users)
+
+// GET single user (/api/users/1)
+
+// POST create user (/api/users)
+
+// PUT update user (/api/users/1)
+
+// DELETE user (/api/users/1)
+
+// POST login ?? 
+
+// POST logout ??
+
+module.exports = router;
