6051ae9b5e9d54a7f5d1ccc6dab84be806f8295a
alirueter <rueter.alison@gmail.com>
2020-08-31 11:23:59 -0500

    heroku not working

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,35 @@
-// import sequelize constructor from library
 const Sequelize = require('sequelize');
-// load dotenv
+
 require('dotenv').config();
 
-let sequelize;
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+module.exports = sequelize;
+
+
+// // import sequelize constructor from library
+// const Sequelize = require('sequelize');
+// // load dotenv
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
 
 // // create connection to db, pass in MySQL information for username & password
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
