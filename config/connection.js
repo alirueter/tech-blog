fbf69f80ba4ec093b903eee5d883bd0fe26c0c5e
alirueter <rueter.alison@gmail.com>
2020-08-30 21:21:00 -0500

    connection updates

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,23 @@ const Sequelize = require('sequelize');
 // load dotenv
 require('dotenv').config();
 
-// create connection to db, pass in MySQL information for username & password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
+}
+
+// // create connection to db, pass in MySQL information for username & password
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
 module.exports = sequelize;
