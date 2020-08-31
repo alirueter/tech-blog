abd3d075fa2cbd123759db29b297659a61f07691
alirueter <rueter.alison@gmail.com>
2020-08-31 12:03:37 -0500

    force sync in server.js

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.use(require('./controllers'));
 
 app.disable('etag');
 
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({force: true}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
