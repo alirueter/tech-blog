1d2aaffd08f18a5aec83e175281452ca31b91f90
alirueter <rueter.alison@gmail.com>
2020-08-30 21:13:35 -0500

    Fixed host name environment variable in connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // create connection to db, pass in MySQL information for username & password
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
 });
